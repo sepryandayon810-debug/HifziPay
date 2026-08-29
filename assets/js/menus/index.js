d9cd70e91b3afded8e820a599b09586aded3381d
sepryandayon810-debug <sepryandayon810@gmail.com>
2026-08-29 12:25:44 +0700

    Update index.js

--- assets/js/menus/index.js
+++ assets/js/menus/index.js
@@ -1,1 +1,12 @@
+import { pulsaMenus } from "./pulsa.js";
+import { listrikMenus } from "./listrik.js";
+import { tagihanMenus } from "./tagihan.js";
+import { digitalMenus } from "./digital.js";
 
+// Satu katalog agar tombol di index.html tetap punya nama menu yang konsisten.
+export const serviceCopy = {
+  ...pulsaMenus,
+  ...listrikMenus,
+  ...tagihanMenus,
+  ...digitalMenus
+};
